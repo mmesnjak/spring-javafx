e5e3f58a924aeb30e382fc7fa50004e8fbfb5df4
mmesnjak <misel.mesnjak@asseco-see.hr>
2019-01-18 14:16:04 +0100

    Initial commit

---
+++ src/main/java/com/example/javafx/JavafxApplication.java
@@ -0,0 +1,53 @@
+package com.example.javafx;
+
+import javafx.application.Application;
+import javafx.application.HostServices;
+import javafx.application.Platform;
+import javafx.stage.Stage;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+
+public class JavafxApplication extends Application {
+
+  private ConfigurableApplicationContext context;
+
+  @Override
+  public void init() throws Exception {
+    ApplicationContextInitializer<GenericApplicationContext> initializer = ac -> {
+      ac.registerBean(Application.class, () -> JavafxApplication.this);
+      ac.registerBean(Parameters.class, this::getParameters);
+      ac.registerBean(HostServices.class, this::getHostServices);
+    };
+
+    this.context = new SpringApplicationBuilder()
+        .sources(BootifulFxApplication.class)
+        .initializers(initializer)
+        .run(getParameters().getRaw().toArray(new String[0]));
+  }
+
+  @Override
+  public void stop() throws Exception {
+    this.context.close();
+    Platform.exit();
+  }
+
+  @Override
+  public void start(Stage stage) throws Exception {
+    this.context.publishEvent(new StageReadyEvent(stage));
+  }
+}
+
+class StageReadyEvent extends ApplicationEvent {
+
+  StageReadyEvent(Stage source) {
+    super(source);
+  }
+
+  Stage getStage() {
+    return (Stage) getSource();
+  }
+}
+
---
+++ src/main/java/com/example/javafx/SimpleUiController.java
@@ -0,0 +1,28 @@
+package com.example.javafx;
+
+import javafx.application.HostServices;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleUiController {
+
+  private final HostServices hostServices;
+
+  public SimpleUiController(HostServices hostServices) {
+    this.hostServices = hostServices;
+  }
+
+  @FXML
+  public Label label;
+
+  @FXML
+  public Button button;
+
+  @FXML
+  public void initialize() {
+    this.button.setOnAction(actionEvent -> this.label.setText(this.hostServices.getDocumentBase()));
+  }
+}
